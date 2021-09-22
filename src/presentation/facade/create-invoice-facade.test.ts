2791feaef218af6e463ddf79cf70f1f9ba0d4581
Cleriston <cleriston.mari@gmail.com>
2021-09-22 09:23:18 +0100

    test: ensure call GetItemProcess with correct value

--- src/presentation/facade/create-invoice-facade.test.ts
+++ src/presentation/facade/create-invoice-facade.test.ts
@@ -1,8 +1,12 @@
 import { CreateInvoiceFacade } from './create-invoice-facade'
 import { GetOperationType } from '../../usercases/protocols/get-operation-type'
+import { ProcessModel } from '../../../domain/processo/model/process'
+import { GetItem } from '../../../domain/get-item'
 
 type SutTypes = {
   sut: CreateInvoiceFacade
+  getOperationType: GetOperationType
+  getItemProcess: GetItem<ProcessModel>
 }
 
 const makeGetOperationTypeStub = (): GetOperationType => {
@@ -14,11 +18,23 @@ const makeGetOperationTypeStub = (): GetOperationType => {
   return new GetOperationTypeStub()
 }
 
+const makeGetItemProcess = (): GetItem<ProcessModel> => {
+  class GetProcessItemStub implements GetItem<ProcessModel> {
+    async get (id: number): Promise<ProcessModel> {
+      return Promise.resolve({ id_processo: 1, numero: 3 })
+    }
+  }
+  return new GetProcessItemStub()
+}
+
 const makeSut = (): SutTypes => {
+  const getItemProcess = makeGetItemProcess()
   const getOperationType = makeGetOperationTypeStub()
-  const sut = new CreateInvoiceFacade(getOperationType)
+  const sut = new CreateInvoiceFacade(getOperationType, getItemProcess)
   return {
-    sut
+    sut,
+    getOperationType,
+    getItemProcess
   }
 }
 
@@ -28,5 +44,19 @@ describe('Create Invoice Facade', () => {
     const facadeSpy = jest.spyOn(sut, 'create')
     await sut.create(1)
     expect(facadeSpy).toHaveBeenCalledWith(1)
+  })
+
+  test('Ensure call GetOperationType with correct value', async () => {
+    const { sut, getOperationType } = makeSut()
+    const getOperationTypeSpy = jest.spyOn(getOperationType, 'get')
+    await sut.create(1)
+    expect(getOperationTypeSpy).toHaveBeenCalledWith(1)
+  })
+
+  test('Ensure call GetItemProcess with correct value', async () => {
+    const { sut, getItemProcess } = makeSut()
+    const getItemProcessSpy = jest.spyOn(getItemProcess, 'get')
+    await sut.create(1)
+    expect(getItemProcessSpy).toHaveBeenCalledWith(1)
   })
 })
