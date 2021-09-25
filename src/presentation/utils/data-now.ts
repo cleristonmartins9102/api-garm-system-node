ad50f3944a30219926a6c7ea0550f423bbe23fe0
Cleriston <cleriston.mari@gmail.com>
2021-09-25 01:15:02 +0100

    feat: create util dateNow

---
+++ src/presentation/utils/data-now.ts
@@ -0,0 +1,5 @@
+const data = new Date()
+const dia = String(data.getDate()).padStart(2, '0')
+const mes = String(data.getMonth() + 1).padStart(2, '0')
+const ano = data.getFullYear()
+export const dateNow = `${dia}/${mes}/${ano}`
